4f1771846187dae24bdf9041ab8a3679bb927679
Zheng Wen <stellar_www@163.com>
2026-03-11 20:22:27 +0800

    eyecandies zero shot

--- run_exps.py
+++ run_exps.py
@@ -8,12 +8,13 @@ if __name__ == "__main__":
     run_file = "train.py"
     train_data_path1 = "/home/node5/AD/datasets/mvtec3d-multiview_672to224"
     train_data_path2 = "/home/node5/AD/datasets/eyecandies-multiview_672to224"
+    save_path = "./exps_compare/mvtec"
 
     dataset1 = "mvtec3d"
     dataset2 = "eyecandies"
     gpu_id = 1
 
-    sh = f'CUDA_VISIBLE_DEVICES={gpu_id} python {run_file} --save_path ./exps_compare/ --train_data_path {train_data_path1} --dataset {dataset1} \
+    sh = f'CUDA_VISIBLE_DEVICES={gpu_id} python {run_file} --save_path {save_path} --train_data_path {train_data_path2} --dataset {dataset2} \
             --epoch 3'
     print(f'exec {sh}')
     # pool.apply_async(os.system, (sh,))
@@ -21,7 +22,7 @@ if __name__ == "__main__":
     for epoch_num in [3]:
         checkpoint = './exps_compare/epoch_{}.pth'.format(epoch_num)
         sh = f'CUDA_VISIBLE_DEVICES={gpu_id} python ./test.py --save_path ./result_compare/ \
-            --checkpoint_path {checkpoint} --data_path {train_data_path2} --dataset {dataset2}'
+            --checkpoint_path {checkpoint} --data_path {train_data_path1} --dataset {dataset1}'
         print(f'exec {sh}')
         pool.apply_async(os.system, (sh,))
 
